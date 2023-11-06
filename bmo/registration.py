bb3f27300dec2c1ff0b3c14f94e70f9177858cfe
Sarah Mouffok <sarah.mouffok@epfl.ch>
2023-11-06 15:24:17 +0000

    rm ElectrophysiologyFeature related types

---
+++ bmo/registration.py
@@ -0,0 +1,231 @@
+import copy
+import json
+import io
+from contextlib import redirect_stdout
+from typing import Optional, Tuple
+
+from kgforge.core import KnowledgeGraphForge, Resource
+
+
+ALREADY_EXISTS_ERROR = " already exists in project"
+ALREADY_DEPRECATED_ERROR = "is deprecated"
+
+SHACL_SCHEMA_ID = "https://bluebrain.github.io/nexus/schemas/shacl-20170720.ttl"
+ONTOLOGY_SCHEMA_ID = "https://neuroshapes.org/dash/ontology"
+CLASS_SCHEMA_ID = "https://neuroshapes.org/dash/ontologyentity"
+
+
+SYNTHETIC_SENTENCES = {
+    "./ontologies/bbp/bmo.ttl": {
+        "synthetic": "./texts/synthetic_texts_bmo.json"
+    },
+    "./ontologies/bbp/data-types.ttl": {
+    },
+    "./ontologies/bbp/efeatures.ttl": {
+        "synthetic": "./texts/synthetic_texts_NeuronElectrophysiologicalFeature.json"
+    },
+    "./ontologies/bbp/mfeatures.ttl": {
+        "synthetic": "./texts/synthetic_texts_NeuronMorphologicalFeature.json"
+    },
+    "./ontologies/bbp/molecular-systems.ttl": {
+        "synthetic": "./texts/synthetic_texts_Molecular_systems.json"
+    },
+    "./ontologies/bbp/speciestaxonomy.ttl": {
+        "synthetic": "./texts/synthetic_texts_Species.json"
+    },
+    "./ontologies/bbp/stimulustypes.ttl": {
+        "synthetic": "./texts/synthetic_texts_ElectricalStimulus.json"
+    },
+    "./ontologies/bbp/celltypes.ttl": {
+        "synthetic": "./texts/synthetic_texts_BrainCellType.json"
+    },
+    "./ontologies/bbp/brainregion.ttl": {
+        "synthetic": "./texts/synthetic_texts_BrainRegion.json",
+        "wiki": "./texts/wiki_texts_BrainRegion.json"
+
+    }
+}
+
+
+def register_class(
+        forge: KnowledgeGraphForge, class_resource: Resource, tag=None
+) -> Tuple[Optional[Exception], Resource]:
+    """Register ontology class to the store."""
+    return _register_update(
+        forge=forge, resource=class_resource,
+        schema_id=CLASS_SCHEMA_ID, tag=tag, type_str="Class"
+    )
+
+
+def register_schema(
+        forge: KnowledgeGraphForge, schema_filepath: str, schema_resource: Resource, tag: str
+) -> Tuple[Optional[Exception], Resource]:
+    """Register schema to the store."""
+    return _register_update(
+        forge, schema_resource, schema_id=SHACL_SCHEMA_ID,
+        tag=tag, extra_message=f"from file {schema_filepath}",
+        raise_on_fail=False, type_str="Schema"
+    )
+
+
+def register_ontology(
+        forge, ontology_json, ontology_filepath, ontology_graph, tag, class_resources_mapped=None
+) -> Tuple[Optional[Exception], Resource]:
+
+    ontology_json = copy.deepcopy(ontology_json)
+    # del ontology_json["@context"]
+    ontology_resource = forge.from_json(ontology_json)
+    dirpath = f"./{ontology_filepath.split('/')[-1].split('.')[0]}"
+    dirpath_ttl = f"{dirpath}.ttl"
+    ontology_graph.serialize(destination=dirpath_ttl, format="ttl")
+    ontology_resource.distribution = [forge.attach(dirpath_ttl, content_type="text/turtle")]
+
+    dirpath_json = f"{dirpath}.json"
+    with open(dirpath_json, "w") as fp:
+        json.dump(ontology_json, fp)
+    ontology_resource.distribution.append(
+        forge.attach(dirpath_json, content_type="application/ld+json"))
+
+    if class_resources_mapped is not None:
+        defined_types_df = forge.as_dataframe(class_resources_mapped)
+        dirpath_csv = f"{dirpath}.csv"
+        defined_types_df.to_csv(dirpath_csv)
+        ontology_resource.distribution.append(forge.attach(dirpath_csv, content_type="text/csv"))
+
+    if ontology_filepath in SYNTHETIC_SENTENCES:
+        synthetic = SYNTHETIC_SENTENCES[ontology_filepath].get("synthetic", None)
+        wiki = SYNTHETIC_SENTENCES[ontology_filepath].get("wiki", None)
+        if synthetic:
+            ontology_resource.distribution.append(forge.attach(synthetic, content_type="text/json"))
+        if wiki:
+            ontology_resource.distribution.append(forge.attach(wiki, content_type="text/json"))
+
+    return _register_update(
+        forge, ontology_resource,
+        schema_id=ONTOLOGY_SCHEMA_ID, tag=tag, type_str="Ontology",
+        extra_message=f"from file {ontology_filepath}"
+    )
+
+
+def _process_already_existing_resource(forge: KnowledgeGraphForge, resource: Resource):
+    resource_json = forge.as_json(resource)
+    resource_id = resource_json.pop("@id", resource_json.pop("id", None))
+    resource_id = forge._model.context().expand(resource_id)
+    # resource_json.pop("@type", resource_json.pop("type", None))
+    resource_updated = forge.from_json(resource_json)
+    existing_resource = forge.retrieve(resource_id)
+    resource_updated.id = existing_resource.id
+    if hasattr(resource, "context"):
+        resource_updated.context = resource.context
+    # resource_updated.type = existing_resource.type
+    resource_updated._store_metadata = existing_resource._store_metadata
+    return resource_updated
+
+
+def _handle_failed(act, resource, action_type, extra_message, type_str, raise_on_fail):
+    message = f"Failed to {action_type} {type_str.lower()}:{resource.get_identifier()} " \
+              f"{extra_message}: {act.message}"
+    print(message)
+    ex = Exception(message)
+    if raise_on_fail:
+        raise ex
+    return ex, resource
+
+
+def deprecate_schema(
+        forge: KnowledgeGraphForge, schema_filename: str, schema_resource: Resource
+) -> Tuple[Optional[Exception], Resource]:
+    """Deprecate schema from the store."""
+    return _deprecate(
+        forge, schema_resource, extra_message=f"from file {schema_filename}",
+        raise_on_fail=False, type_str="Schema"
+    )
+
+
+def _successful_action_output(type_str, resource, action_verb):
+    return f"{type_str} {resource.get_identifier()} has been successfully {action_verb}."
+
+
+def _deprecate(
+        forge: KnowledgeGraphForge, resource: Resource,
+        type_str, extra_message="", raise_on_fail=True
+):
+    resource_retrieved = forge.retrieve(resource.get_identifier())
+
+    if resource_retrieved is None:
+        print(
+            f"{type_str} {resource.get_identifier()} "
+            f"cannot be deprecated because it cannot be retrieved"
+        )
+        return None, resource
+
+    with redirect_stdout(io.StringIO()):
+        forge.deprecate(resource_retrieved)
+
+    last_action = resource_retrieved._last_action
+
+    if not last_action:
+        print("No deprecation action?")
+        return None, resource_retrieved
+
+    if last_action.succeeded:
+        print(_successful_action_output(type_str, resource_retrieved, "deprecated"))
+        return None, resource_retrieved
+
+    if ALREADY_DEPRECATED_ERROR in last_action.message:
+        print(f"{type_str} {resource_retrieved.get_identifier()} already deprecated.")
+        return None, resource_retrieved
+
+    return _handle_failed(
+        last_action, resource_retrieved, "deprecate", extra_message, type_str, raise_on_fail
+    )
+
+
+def _register_update(
+        forge: KnowledgeGraphForge, resource: Resource, schema_id: str, tag: str,
+        type_str, extra_message="", raise_on_fail=True
+) -> Tuple[Optional[Exception], Resource]:
+
+    with redirect_stdout(io.StringIO()):
+        forge.register(resource, schema_id=schema_id)
+
+    last_action = resource._last_action
+
+    if not last_action:
+        print("No registration action?")
+        return None, resource
+
+    if last_action.succeeded:
+        print(_successful_action_output(type_str, resource, "registered"))
+
+        if tag is not None:
+            forge.tag(resource, tag)
+        return None, resource
+
+    if ALREADY_EXISTS_ERROR not in last_action.message:
+        return _handle_failed(
+            last_action, resource, "register", extra_message, type_str, raise_on_fail
+        )
+
+    print(f"{type_str} {resource.get_identifier()} already exists, updating...")
+    resource_updated = _process_already_existing_resource(forge, resource)
+
+    with redirect_stdout(io.StringIO()):
+        forge.update(resource_updated)
+
+    updated_last_action = resource_updated._last_action
+
+    if not updated_last_action:
+        print("No update action?")
+        return None, resource
+
+    if updated_last_action.succeeded:
+        print(_successful_action_output(type_str, resource, "updated"))
+
+        if tag is not None:
+            forge.tag(resource_updated, tag)
+        return None, resource
+
+    return _handle_failed(
+        updated_last_action, resource_updated, "update", extra_message, type_str, raise_on_fail
+    )
--- tests/conftest.py
+++ tests/conftest.py
@@ -4,20 +4,21 @@ import pytest
 import rdflib
 from kgforge.core import KnowledgeGraphForge, Resource
 from rdflib import RDFS, Namespace, RDF, OWL
-from rdflib.plugins.parsers.notation3 import BadSyntax
 import bmo.ontologies as bmo
 from bmo.utils import BMO, MBA, NXV, SCHEMAORG
-from register_ontologies import _merge_ontology, load_ontologies, load_schemas, initialise_graph
+from register_ontologies import _merge_ontology, _initialize_forge_objects
+from bmo.loading import load_ontologies, load_schemas
 
 
 def pytest_addoption(parser):
-    parser.addoption("--environment", action="store", default="https://staging.nise.bbp.epfl.ch/nexus/v1")
+    parser.addoption("--environment", action="store",
+                     default="https://staging.nise.bbp.epfl.ch/nexus/v1")
     parser.addoption("--bucket", action="store", default="neurosciencegraph/datamodels")
-    parser.addoption("--atlas_parcellation_ontology", action="store", default="https://bbp.epfl.ch/neurosciencegraph/data/ontologies/34388d3b-0b88-4deb-9686-6fcd9ef8990e")
+    parser.addoption("--atlas_parcellation_ontology", action="store",
+                     default="https://bbp.epfl.ch/neurosciencegraph/data/ontologies/34388d3b-0b88-4deb-9686-6fcd9ef8990e")
     parser.addoption("--atlas_parcellation_ontology_version", action="store", default=None)
     parser.addoption("--atlas_parcellation_ontology_bucket", action="store", default="bbp/atlas")
     parser.addoption("--token", action="store")
-    
 
 
 PREFIX_MAPPINGS = {
@@ -78,9 +79,11 @@ def bucket(pytestconfig):
 def atlas_parcellation_ontology(pytestconfig):
     return pytestconfig.getoption("atlas_parcellation_ontology")
 
+
 @pytest.fixture(scope="session")
 def atlas_parcellation_ontology_version(pytestconfig):
     return pytestconfig.getoption("atlas_parcellation_ontology_version")
+
 
 @pytest.fixture(scope="session")
 def atlas_parcellation_ontology_bucket(pytestconfig):
@@ -103,47 +106,58 @@ def transformed_schema_path(pytestconfig):
 
 
 @pytest.fixture(scope="session")
-def forge(environment, bucket, token):
-    forge = KnowledgeGraphForge(
-        "./config/forge-config-new.yml",
-        endpoint=environment, bucket=bucket, token=token, debug=True)
-    return forge
+def forge_objects(environment, bucket, token, atlas_parcellation_ontology_bucket):
+    return _initialize_forge_objects(
+        endpoint=environment, input_bucket=bucket, token=token,
+        atlas_parcellation_ontology_bucket=atlas_parcellation_ontology_bucket
+    )
+
 
 @pytest.fixture(scope="session")
-def forge_atlas(environment, atlas_parcellation_ontology_bucket, token):
-    forge = KnowledgeGraphForge(
-        "./config/forge-config-new.yml",
-        endpoint=environment, bucket=atlas_parcellation_ontology_bucket, token=token, debug=True)
-    return forge
+def forge(forge_objects):
+    return forge_objects[0]
+
 
 @pytest.fixture(scope="session")
-def forge_schema(environment, bucket, token):
-    forge = KnowledgeGraphForge(
-        "./config/forge-schema-config.yml",
-        endpoint=environment, bucket=bucket, token=token, debug=True)
-    return forge
+def forge_atlas(forge_objects):
+    return forge_objects[2]
+
+
+@pytest.fixture(scope="session")
+def forge_schema(forge_objects):
+    return forge_objects[1]
 
 
 @pytest.fixture(scope="session")
 def data_jsonld_context(forge, all_ontology_graphs):
     forge_context = forge._model.context()
-    new_jsonld_context, errors = bmo.build_context_from_ontology(all_ontology_graphs[0], forge_context)
+    new_jsonld_context, errors = bmo.build_context_from_ontology(all_ontology_graphs[0],
+                                                                 forge_context)
     return new_jsonld_context, errors
 
 
 @pytest.fixture(scope="session")
 def all_ontology_graphs(ontology_dir):
+    ontology_pattern = f"{ontology_dir}/*.ttl"
+    ontology_files = glob.glob(ontology_pattern)
+
     try:
-        all_ontology_graphs = initialise_graph()
-        ontology_graphs_dict = load_ontologies(ontology_dir, all_ontology_graphs)
+        ontology_graphs_dict, all_ontology_graphs = load_ontologies(ontology_pattern)
         assert len(all_ontology_graphs) > 0
-        ontology_files = glob.glob(f"{ontology_dir}/*.ttl")
         assert len(ontology_files) > 0
         assert len(ontology_graphs_dict) == len(ontology_files)
-        assert len(ontology_graphs_dict) == len(list(all_ontology_graphs.subjects(RDF.type, OWL.Ontology)))
+        assert len(ontology_graphs_dict) == len(
+            list(all_ontology_graphs.subjects(RDF.type, OWL.Ontology)))
         return all_ontology_graphs, ontology_graphs_dict
     except Exception as e:
-        pytest.fail(f"Failed to load all ontologies in {ontology_dir}. Not loaded ontologies are: {set([filepath.split('/')[-1] for filepath in ontology_files]) - set([filepath.split('/')[-1] for filepath in ontology_graphs_dict.keys()])}: {e}")
+        missing_ontologies = set([filepath.split('/')[-1] for filepath in ontology_files]) - \
+                             set([filepath.split('/')[-1] for filepath in
+                                  ontology_graphs_dict.keys()])
+
+        pytest.fail(
+            f"Failed to load all ontologies in {ontology_dir}. "
+            f"Not loaded ontologies are: {missing_ontologies}: {e}"
+        )
 
 
 @pytest.fixture(scope="session")
@@ -152,34 +166,50 @@ def framed_classes(data_jsonld_context, all_ontology_graphs, atlas_hierarchy_ont
     assert len(errors) == 0
 
     ontology_graph = all_ontology_graphs[0]
-    ontology_graphs_dict =  all_ontology_graphs[1]
-    
+    ontology_graphs_dict = all_ontology_graphs[1]
+
     brain_region_graph = ontology_graphs_dict["./ontologies/bbp/brainregion.ttl"]
 
     atlas_ontology_graph = atlas_hierarchy_ontology_graph[0]
     atlas_hierarchy = atlas_hierarchy_ontology_graph[1]
 
-    triples_to_add, triples_to_remove = _merge_ontology(atlas_ontology_graph, brain_region_graph, 
-                                                        ontology_graph, [SCHEMAORG.hasPart, SCHEMAORG.isPartOf, RDFS.label, MBA.atlas_id, MBA.color_hex_triplet,
-                                                                        MBA.graph_order, MBA.hemisphere_id, MBA.st_level, SCHEMAORG.identifier, BMO.representedInAnnotation,
-                                                                        BMO.regionVolumeRatioToWholeBrain, BMO.regionVolume]
-                                                        )
-    class_ids, class_jsons, all_blank_node_triples, new_classes = bmo.frame_classes(ontology_graph, new_jsonld_context, new_jsonld_context.document,
-                                                                                    atlas_hierarchy.atlasRelease.id, atlas_hierarchy.atlasRelease._rev)
-    return class_ids, class_jsons, ontology_graph, triples_to_add, triples_to_remove, atlas_hierarchy.atlasRelease.id, atlas_hierarchy.atlasRelease._rev
+    triples_to_add, triples_to_remove = _merge_ontology(
+        atlas_ontology_graph, brain_region_graph,
+        ontology_graph, [
+            SCHEMAORG.hasPart,
+            SCHEMAORG.isPartOf, RDFS.label, MBA.atlas_id, MBA.color_hex_triplet,
+            MBA.graph_order, MBA.hemisphere_id, MBA.st_level, SCHEMAORG.identifier,
+            BMO.representedInAnnotation, BMO.regionVolumeRatioToWholeBrain, BMO.regionVolume
+        ]
+    )
+    class_ids, class_jsons, all_blank_node_triples, new_classes = bmo.frame_classes(
+        ontology_graph, new_jsonld_context, new_jsonld_context.document,
+        atlas_hierarchy.atlasRelease.id, atlas_hierarchy.atlasRelease._rev
+    )
+    return (
+        class_ids, class_jsons, ontology_graph, triples_to_add, triples_to_remove,
+        atlas_hierarchy.atlasRelease.id, atlas_hierarchy.atlasRelease._rev
+    )
 
 
 @pytest.fixture(scope="session")
-def atlas_hierarchy_ontology_graph(atlas_parcellation_ontology, atlas_parcellation_ontology_version, forge_atlas):
+def atlas_hierarchy_ontology_graph(atlas_parcellation_ontology, atlas_parcellation_ontology_version,
+                                   forge_atlas):
     try:
-        version = int(atlas_parcellation_ontology_version) if atlas_parcellation_ontology_version is not None else atlas_parcellation_ontology_version
+        version = int(
+            atlas_parcellation_ontology_version) if atlas_parcellation_ontology_version is not None else atlas_parcellation_ontology_version
         atlas_hierarchy = forge_atlas.retrieve(atlas_parcellation_ontology, version=version)
-        assert hasattr(atlas_hierarchy,"atlasRelease")
+        assert hasattr(atlas_hierarchy, "atlasRelease")
         assert hasattr(atlas_hierarchy.atlasRelease, "_rev")
-        atlas_hierarchy_jsonld_distribution = [distrib for distrib in atlas_hierarchy.distribution if distrib.encodingFormat=="application/ld+json"]
+        atlas_hierarchy_jsonld_distribution = [
+            distrib for distrib in atlas_hierarchy.distribution
+            if distrib.encodingFormat == "application/ld+json"
+        ]
         atlas_hierarchy_jsonld_distribution = atlas_hierarchy_jsonld_distribution[0]
-        forge_atlas.download(atlas_hierarchy_jsonld_distribution, follow="contentUrl", path=".", overwrite=True)
-        atlas_hierarchy_ontology_graph = rdflib.Graph().parse(atlas_hierarchy_jsonld_distribution.name, format="json-ld")
+        forge_atlas.download(atlas_hierarchy_jsonld_distribution, follow="contentUrl", path=".",
+                             overwrite=True)
+        atlas_hierarchy_ontology_graph = rdflib.Graph().parse(
+            atlas_hierarchy_jsonld_distribution.name, format="json-ld")
         assert len(atlas_hierarchy_ontology_graph) > 0
         return atlas_hierarchy_ontology_graph, atlas_hierarchy
     except Exception as e:
@@ -188,25 +218,36 @@ def atlas_hierarchy_ontology_graph(atlas_parcellation_ontology, atlas_parcellati
 
 @pytest.fixture(scope="session")
 def all_schema_graphs(transformed_schema_path, schema_dir, forge_schema):
+    schema_pattern = f"{schema_dir}/**/*.json"
+    recursive = True
+    schema_filenames = glob.glob(schema_pattern, recursive=recursive)
+
+    # non_deprecated_schemas = [
+    #     schema_filename for schema_filename in schema_filenames
+    #     if not json.load(open(schema_filename, 'r')).get("owl:deprecated", False)
+    # ]
+
     try:
-        recursive = True
-        schema_files = glob.glob(f"{schema_dir}/**/*.json", recursive=recursive)
-        all_schema_graphs = initialise_graph()
-        schema_graphs_dict, schema_id_to_filepath_dict = load_schemas(f"{schema_dir}/**/*.json", transformed_schema_path, forge_schema, all_schema_graphs, recursive, save_transformed_schema=False)
+        schema_graphs_dict, schema_id_to_filepath_dict, all_schema_graphs = load_schemas(
+            schema_pattern, transformed_schema_path, forge_schema, recursive,
+            save_transformed_schema=False
+        )
         assert len(all_schema_graphs) > 0
         assert len(schema_id_to_filepath_dict) == len(schema_id_to_filepath_dict)
 
-        assert len(schema_files) > 0
-        assert len(schema_graphs_dict) == len(schema_files)
-        assert len(schema_graphs_dict) == len(list(all_schema_graphs.subjects(RDF.type, NXV.Schema)))
+        assert len(schema_filenames) > 0
+        assert len(schema_graphs_dict) == len(schema_filenames)
+        assert len(schema_graphs_dict) == len(
+            list(all_schema_graphs.subjects(RDF.type, NXV.Schema)))
         all_imported_schemas = all_schema_graphs.objects(None, OWL.imports)
         all_imported_schemas = {str(r) for r in all_imported_schemas}
         loaded_schema = set(schema_id_to_filepath_dict.keys())
         assert len(loaded_schema) >= len(set(all_imported_schemas))
-        assert all_imported_schemas.issubset(loaded_schema), f"Imported schemas are missing: {all_imported_schemas - loaded_schema}"
+        assert all_imported_schemas.issubset(
+            loaded_schema), f"Imported schemas are missing: {all_imported_schemas - loaded_schema}"
         schema_id_to_filepath_dict_values = list(schema_id_to_filepath_dict.values())
         for k, v in schema_graphs_dict.items():
-            assert k in schema_files
+            assert k in schema_filenames
             assert k in schema_id_to_filepath_dict_values
             assert len(v) == 5
             assert "resource" in v
@@ -218,4 +259,9 @@ def all_schema_graphs(transformed_schema_path, schema_dir, forge_schema):
             assert "jsonld" in v
         return all_schema_graphs, schema_graphs_dict, schema_id_to_filepath_dict
     except Exception as e:
-        pytest.fail(f"Failed to load all schemas in {schema_dir}. Not loaded schemas are {set([filepath.split('/')[-1] for filepath in schema_files]) - set([filepath.split('/')[-1] for filepath in schema_graphs_dict.keys()])}. {e}")
+        missing_schemas = set([filepath.split('/')[-1] for filepath in schema_filenames]) - \
+                          set([filepath.split('/')[-1] for filepath in schema_graphs_dict.keys()])
+        pytest.fail(
+            f"Failed to load all schemas in {schema_dir}. "
+            f"Not loaded schemas are {missing_schemas}. {e}"
+        )
