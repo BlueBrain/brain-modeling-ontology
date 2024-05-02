022a47751322a04c061c0c33444af685fb9f099b
Mohameth François Sy <mohameth.sy@epfl.ch>
2024-05-02 05:08:22 +0000

    Update slim ont and schemas  * Kept only one label for slim ont  * Used bmo as ns for BrainLayer instead of nsg in common brainLocation schema  * Filtering BrainLayer in common brainLocation schema transitively with  * Added has_rank as cls property to collect in speciestaxonomy_slim ont

--- bmo/slim_ontologies.py
+++ bmo/slim_ontologies.py
@@ -3,16 +3,29 @@ from rdflib import RDF, Graph, term, RDFS, OWL
 from bmo.loading import initialise_graph
 
 
-SLIM_CLASS_ATTRIBUTES = ['@id', '@type', 'label', 'subClassOf', 'deprecated']
-SLIM_GRAPH_PREDICATES = [RDF.type, RDFS.label, RDFS.subClassOf,
-                         term.URIRef("https://neuroshapes.org/defines"),
-                         term.URIRef("http://purl.obolibrary.org/obo/ncbitaxon#has_rank")]
+SLIM_CLASS_ATTRIBUTES = [
+    "@id",
+    "@type",
+    "label",
+    "subClassOf",
+    "deprecated",
+    "has_rank",
+]
+SLIM_GRAPH_PREDICATES = [
+    RDF.type,
+    RDFS.label,
+    RDFS.subClassOf,
+    term.URIRef("https://neuroshapes.org/defines"),
+    term.URIRef("http://purl.obolibrary.org/obo/ncbitaxon#has_rank"),
+]
 SLIM_GRAPH_TYPES = [OWL.Ontology, OWL.Class, OWL.NamedIndividual]
 
 
-def create_slim_ontology_graph(original_graph: Graph,
-                               keep_attributes: List[term.URIRef] = SLIM_GRAPH_PREDICATES,
-                               keep_types: List[term.URIRef] = SLIM_GRAPH_TYPES) -> Graph:
+def create_slim_ontology_graph(
+    original_graph: Graph,
+    keep_attributes: List[term.URIRef] = SLIM_GRAPH_PREDICATES,
+    keep_types: List[term.URIRef] = SLIM_GRAPH_TYPES,
+) -> Graph:
     """Remove any property from the classes that is not in the provided list of attributes.
 
     :param source_graph: The original graph to be slim
@@ -34,19 +47,26 @@ def create_slim_ontology_graph(original_graph: Graph,
     return slim_graph
 
 
-def create_slim_classes(original_classes: List[Dict],
-                        keep_attributes: List[str] = SLIM_CLASS_ATTRIBUTES) -> List[Dict]:
+def create_slim_classes(
+    original_classes: List[Dict], keep_attributes: List[str] = SLIM_CLASS_ATTRIBUTES
+) -> List[Dict]:
     """Remove any property from the classes that is not in the provided list of attributes.
 
     :param original_classes: the list of classes to be slim
     :param keep_attributes: a list with predicates to keep in the class
     """
-    new_classes = [{k: v for k, v in oclass.items() if k in keep_attributes}
-                   for oclass in original_classes]
+    new_classes = [
+        {k: v for k, v in oclass.items() if k in keep_attributes}
+        for oclass in original_classes
+    ]
     return new_classes
 
 
 def get_slim_ontology_id(original_id: term.URIRef) -> term.URIRef:
     """Add '_slim' to the usual name of the ontology id"""
-    base_uri = str(original_id) if not str(original_id).endswith("/") else str(original_id)[:-1]
+    base_uri = (
+        str(original_id)
+        if not str(original_id).endswith("/")
+        else str(original_id)[:-1]
+    )
     return term.URIRef(f"{base_uri}_slim")
